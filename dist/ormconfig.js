541979bbdeb0edb312075c734d737bf6d90b7e6f
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-15 15:14:55 +0300

    nestjs project

--- dist/ormconfig.js
+++ dist/ormconfig.js
@@ -1,14 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const comment_entity_1 = require("./src/entities/comment.entity");
+const topic_entity_1 = require("./src/entities/topic.entity");
 const user_entity_1 = require("./src/entities/user.entity");
 const config = {
-    type: 'postgres',
-    database: 'testDB',
-    host: 'localhost',
+    type: "postgres",
+    database: "testDB",
+    host: "localhost",
     port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    entities: [user_entity_1.User],
+    username: "postgres",
+    password: "postgres",
+    entities: [user_entity_1.User, topic_entity_1.Topic, comment_entity_1.Comment],
     synchronize: true,
 };
 exports.default = config;
