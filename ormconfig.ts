e553ff2ff33e8b5d129dc2b23ad0c3229103b2df
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-15 15:15:13 +0300

    nestjs project

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,16 @@
-import { User } from 'src/entities/user.entity';
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { Comment } from "src/entities/comment.entity";
+import { Topic } from "src/entities/topic.entity";
+import { User } from "src/entities/user.entity";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 
 const config: PostgresConnectionOptions = {
-  type: 'postgres',
-  database: 'testDB',
-  host: 'localhost',
+  type: "postgres",
+  database: "testDB",
+  host: "localhost",
   port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  entities: [User], // we are passing the UserModule here
+  username: "postgres",
+  password: "postgres",
+  entities: [User, Topic, Comment], // we are passing the UserModule here, if we have other entities we will have to add them in as well
   synchronize: true,
 };
 
